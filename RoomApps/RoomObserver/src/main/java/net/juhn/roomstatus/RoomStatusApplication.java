8846d2d69d9fd71de39aa029ac49fe3f8913a93e
Sascha <sascha@juhn.net>
2020-09-04 16:27:43 +0200

    Added PID File writer

--- RoomApps/RoomObserver/src/main/java/net/juhn/roomstatus/RoomStatusApplication.java
+++ RoomApps/RoomObserver/src/main/java/net/juhn/roomstatus/RoomStatusApplication.java
@@ -2,12 +2,15 @@ package net.juhn.roomstatus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class RoomStatusApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RoomStatusApplication.class, args);
+    	SpringApplication springApplication = new SpringApplication(RoomStatusApplication.class);
+    	springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 
 }
