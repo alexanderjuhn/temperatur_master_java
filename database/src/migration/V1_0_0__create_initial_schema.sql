08ecb49a6a49f309735e0bcb9ddf52625c52fe63
Alexander Juhn <home@juhn.net>
2021-05-06 22:43:18 +0200

    refs #8 created Flyway config and database starter scripts

---
+++ database/src/migration/V1_0_0__create_initial_schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE statusdata.room (
+	id int4 NOT NULL,
+	"name" varchar(32) NOT NULL,
+	CONSTRAINT room_pk PRIMARY KEY (id)
+);
+
+
+CREATE TABLE statusdata.roomdata (
+	id int4 NOT NULL,
+	room_id int4 NOT NULL,
+	temperature numeric(4,1) NULL DEFAULT 0,
+	humidity numeric(4,1) NULL DEFAULT 0,
+	datecreated timestamp NOT NULL,
+	CONSTRAINT roomdata_pk PRIMARY KEY (id)
+);
+
+
+-- statusdata.roomdata foreign keys
+
+ALTER TABLE statusdata.roomdata ADD CONSTRAINT roomdata_fk FOREIGN KEY (room_id) REFERENCES statusdata.room(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
---
+++ database/src/migration/V1_1_0__extend_roomdata.sql
@@ -0,0 +1,1 @@
+ ALTER TABLE statusdata.roomdata ADD pressure int2 NULL;
